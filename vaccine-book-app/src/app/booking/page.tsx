79abe900527646762a1354e541c53914906088f8
Thuwa Ngeonsalung <pai.tuva@gmail.com>
2023-08-30 18:44:53 +0700

    Assignment04
    Updated Version
    Props, Layouts, Pages, Styles

--- vaccine-book-app/src/app/booking/page.tsx
+++ vaccine-book-app/src/app/booking/page.tsx
@@ -1,7 +1,7 @@
 export default function Booking() {
   return (
-    <main>
-      <div className="text-xl text-center font-serif italic font-semibold">
+    <main className="relative bg-gray h-screen">
+      <div className="relative top-[120px] text-xl text-center font-serif italic font-semibold">
         Booking
       </div>
     </main>
