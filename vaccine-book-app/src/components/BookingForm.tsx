0ab0c0c0350aedfe6ad2494e5af9ae7f14e27cf6
Thuwa Ngeonsalung <pai.tuva@gmail.com>
2023-09-07 16:36:30 +0700

    Assignment5
    For assignment week 05, version 1.1
    Add submit button to reservation form

--- vaccine-book-app/src/components/BookingForm.tsx
+++ vaccine-book-app/src/components/BookingForm.tsx
@@ -72,6 +72,16 @@ export default function BookingForm() {
       <div className="flex flex-wrap -mx-3 mb-6">
         <LocationDateReserve />
       </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
